8e2aa2b2d161988f3a47ef8c3138f22d77451620
Anthony Keith <jt1keith@gmail.com>
2023-02-14 20:59:18 -0800

    added form functionality

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict'
+
+let form = document.querySelector('form');
+
+function confirmAppointment(name, email, photographer, date){
+  let reminder = document.getElementById('reminder');
+  let confirmation = document.createElement('p')
+  confirmation.innerHTML = `${name}, your appointment with ${photographer} is on ${date}. We sent a confirmation email to ${email}`
+  reminder.appendChild(confirmation);
+}
+let handlSubmit = function(event) {
+  event.preventDefault();
+  let clientName = event.target.name.value;
+  let clientEmail = event.target.email.value;
+  let photographerChoice = event.target.photographer.value;
+  let appointmentDate = event.target.appointment.value;
+  confirmAppointment(clientName, clientEmail, photographerChoice, appointmentDate);
+}
+
+form.addEventListener('submit', handlSubmit);
